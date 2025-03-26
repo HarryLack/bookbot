0724ba276ee206e860fc61cfb4d176e59bb2dfb8
Harry B-L <harrylackdev@gmail.com>
2025-03-26 20:09:29 +0000

    C3L1

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-from stats import count_unique_chars, count_words
+from stats import chars_dict_to_sorted_list, count_unique_chars, count_words
 
 
 def get_book_text(path: str):
     with open(path) as f:
         return f.read()
-
-
-def sort_by(dict):
-    return dict["count"]
 
 
 def main():
@@ -17,18 +13,13 @@ def main():
     word_count = count_words(file_contents)
     chars = count_unique_chars(file_contents)
 
-    chars_list = []
-
-    for c in chars:
-        chars_list.append({'char': c, 'count': chars[c]})
-
-    chars_list.sort(reverse=True, key=sort_by)
+    chars_list = chars_dict_to_sorted_list(chars)
 
     print(f"---- {book_path} ----")
-    print(f"{word_count} words found in the document")
+    print(f"Found {word_count} total words")
     for c in chars_list:
         if c["char"].isalpha():
-            print(f"The '{c['char']}' character was found {c['count']} times")
+            print(f"{c['char']}: {c['count']}")
     print(f"---- End ----")
 
 
--- stats.py
+++ stats.py
@@ -12,3 +12,15 @@ def count_unique_chars(text: str):
         else:
             chars[c] = 1
     return chars
+
+
+def sort_by(dict):
+    return dict["count"]
+
+
+def chars_dict_to_sorted_list(chars_dict):
+    chars_list = []
+    for c in chars_dict:
+        chars_list.append({'char': c, 'count': chars_dict[c]})
+    chars_list.sort(reverse=True, key=sort_by)
+    return chars_list
